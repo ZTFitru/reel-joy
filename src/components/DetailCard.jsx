2d0fcc725c36b9f184d76111b9178196a6859f88
Zelalem Fitru <zelalemtfitru@gmail.com>
2025-01-29 19:43:44 -0500

    Update: bg color for detail section

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { CiHeart } from "react-icons/ci";
 
 const DetailCard = () => {
   const { id } = useParams();
@@ -54,41 +53,44 @@ const DetailCard = () => {
           />
         </div>
 
-        <div className="text-white space-y-4 py-20 sm:py-10 md:py-20">
-          <h2 className="text-3xl font-bold">{clickedMovie.title}</h2>
-          <p className="text-lg">
-            <span className="font-semibold">Rating: </span>
-            {clickedMovie.vote_average}
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Overview: </span>
-            {clickedMovie.overview}
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Genre: </span>
-            {clickedMovie.genres.map((genre) => genre.name).join(", ")}
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Release Date: </span>
-            {clickedMovie.release_date}
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Runtime: </span>
-            {clickedMovie.runtime} mins
-          </p>
-          {clickedMovie.tagline && (
-            <p className="italic text-lg text-gray-300">
-              "{clickedMovie.tagline}"
+        <div className="relative">
+          <div className="absolute inset-0 bg-black/60 rounded-lg"></div>
+          <div className="relative text-white space-y-4 py-20 sm:py-10 md:py-20 px-6 bg-gray-900/80 rounded-lg shadow-lg">
+            <h1 className="text-3xl font-bold">{clickedMovie.title}</h1>
+            <p className="text-lg">
+              <span className="font-semibold">Rating: </span>
+              {clickedMovie.vote_average}
             </p>
-          )}
-          <div className="mt-6">
-            <Link
-              to={`/movies/${clickedMovie.id}/videos`}
-              onClick={showVideo}
-              className="mt-8 px-6 py-2 bg-gray-700 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition-colors"
-            >
-              Trailers & Clips
-            </Link>
+            <p className="text-lg">
+              <span className="font-semibold">Overview: </span>
+              {clickedMovie.overview}
+            </p>
+            <p className="text-lg">
+              <span className="font-semibold">Genre: </span>
+              {clickedMovie.genres.map((genre) => genre.name).join(", ")}
+            </p>
+            <p className="text-lg">
+              <span className="font-semibold">Release Date: </span>
+              {clickedMovie.release_date}
+            </p>
+            <p className="text-lg">
+              <span className="font-semibold">Runtime: </span>
+              {clickedMovie.runtime} mins
+            </p>
+            {clickedMovie.tagline && (
+              <h2 className="italic text-lg text-gray-300">
+                "{clickedMovie.tagline}"
+              </h2>
+            )}
+            <div className="mt-6">
+              <Link
+                to={`/movies/${clickedMovie.id}/videos`}
+                onClick={showVideo}
+                className="mt-8 px-6 py-2 bg-gray-700 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition-colors"
+              >
+                Trailers & Clips
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/components/TvDetail.jsx
+++ src/components/TvDetail.jsx
@@ -52,49 +52,47 @@ const TvDetail = () => {
           />
         </div>
 
-        <div className="text-white space-y-4 py-20 sm:py-10 md:py-20">
-          <h2 className="text-3xl font-bold">{clickedTv.title}</h2>
-          <p className="text-lg">
-            <span className="font-semibold">Rating: </span>
-            {clickedTv.vote_average.toFixed(1)} ⭐️
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Overview: </span>
-            {clickedTv.overview}
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Genre: </span>
-            {clickedTv.genres.map((genre) => genre.name).join(", ")}
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">First Episode: </span>
-            {clickedTv.first_air_date}
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Last Episode: </span>
-            {clickedTv.last_air_date}
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Seasons: </span>
-            {clickedTv.number_of_seasons}
-          </p>
-          <p className="text-lg">
-            <span className="font-semibold">Status: </span>
-            {clickedTv.status}
-          </p>
-          {clickedTv.tagline && (
-            <p className="italic text-lg text-gray-300">
-              "{clickedTv.tagline}"
+        <div className="relative">
+          <div className="absolute inset-0 bg-black/60 rounded-lg"></div>{" "}
+          <div className="relative text-white space-y-4 py-20 sm:py-10 md:py-20 px-6 bg-gray-900/80 rounded-lg shadow-lg">
+            <h2 className="text-3xl font-bold">{clickedTv.title}</h2>
+            <p className="text-lg">
+              <span className="font-semibold">Rating: </span>
+              {clickedTv.vote_average.toFixed(1)} ⭐️
             </p>
-          )}
-          <div className="mt-6">
-            <Link
-              to={`/tv/${clickedTv.id}/videos`}
-              onClick={showVideo}
-              className="mt-8 px-6 py-2 bg-gray-700 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition-colors"
-            >
-              Trailers & Clips
-            </Link>
+            <p className="text-lg">
+              <span className="font-semibold">Overview: </span>
+              {clickedTv.overview}
+            </p>
+            <p className="text-lg">
+              <span className="font-semibold">Genre: </span>
+              {clickedTv.genres.map((genre) => genre.name).join(", ")}
+            </p>
+            <p className="text-lg">
+              <span className="font-semibold">First Episode: </span>
+              {clickedTv.first_air_date}
+            </p>
+            <p className="text-lg">
+              <span className="font-semibold">Last Episode: </span>
+              {clickedTv.last_air_date}
+            </p>
+            <p className="text-lg">
+              <span className="font-semibold">Seasons: </span>
+              {clickedTv.number_of_seasons}
+            </p>
+            <p className="text-lg">
+              <span className="font-semibold">Status: </span>
+              {clickedTv.status}
+            </p>
+            <div className="mt-6">
+              <Link
+                to={`/movies/${clickedTv.id}/videos`}
+                onClick={showVideo}
+                className="mt-8 px-6 py-2 bg-gray-700 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 transition-colors"
+              >
+                Trailers & Clips
+              </Link>
+            </div>
           </div>
         </div>
       </div>
