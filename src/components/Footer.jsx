cabd9b8ab47fc8d592ac8a910ee012f71d3978ea
Zelalem Fitru <zelalemtfitru@gmail.com>
2025-01-31 15:04:19 -0500

    Update: footer icon path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { FcFilmReel } from "react-icons/fc";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-transparent backdrop-blur-md py-4">
       <div className="container mx-auto flex justify-center items-center text-white text-lg">
-        <FcFilmReel className="text-2xl mr-2" />
+        <Link to='/'>
+            <FcFilmReel className="text-2xl mr-2" />
+        </Link>
         <p>&copy; {new Date().getFullYear()} Movie App. All Rights Reserved.</p>
       </div>
     </footer>
